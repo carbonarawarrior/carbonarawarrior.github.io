5f973c31520a92e2e78e75bed314905ca9458e66
theodorepadden <theodorepadden@gmail.com>
2025-06-05 13:30:57 -0400

    i cant figure out how to display markdown . . .

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+import {MarkdownBlock, MarkdownSpan, MarkdownElement} from "https://md-block.verou.me/md-block.js";
+
 function toggleTheme() {
   const stylesheet = document.getElementById("stylesheet");
   const currentTheme = stylesheet.getAttribute("href");
+  const toggleButton = document.getElementById("toggleButton")
   
   if (currentTheme === "gruvbox-light.css") {
-    stylesheet .setAttribute("href", "gruvbox-dark.css");
+    stylesheet.setAttribute("href", "gruvbox-dark.css");
+    toggleButton.innerHTML = "Dark"
   } else {
-    stylesheet .setAttribute("href", "gruvbox-light.css");
+    stylesheet.setAttribute("href", "gruvbox-light.css");
+    toggleButton .innerHTML = "Light"
   }
 }
 
